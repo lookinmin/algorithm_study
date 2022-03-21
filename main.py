11be2dbfffcfc0f13a79ecab72e1343d172bd748
MINSU <ancx1234@naver.com>
2022-03-21 09:57:53 +0900

    0321
    make Branch

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
 
+n1, n2, n3 = map(int, input().split())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if n1 == n2 == n3:
+    print(10000 + n1*1000)
+elif n1 == n2:
+    print(1000 + n1 * 100)
+elif n2 == n3:
+    print(1000 + n2 * 100)
+elif n1 == n3:
+    print(1000 + n3 * 100)
+else :
+    print(max(n1, n2, n3) * 100)
